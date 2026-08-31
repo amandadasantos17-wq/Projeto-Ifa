4a7251d30b9067bc17c74b26d13074167c0cc9f9
Amanda Bento da Paz Santos <amanda.da.santos17@escola.pr.gov.br>
2026-08-30 23:36:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -287,7 +287,7 @@ function trocarImagem(escolha) {
      case "6":
           urlNova = 'café.png';
           textoprincipal.innerText="Como explicar o Sol?";
-          texto.innerText="O Sol é uma estrela gigante feita de gás quente que fica no centro do nosso Sistema Solar, a 150 milhões de quilômetros da Terra. Ele dá luz e calor ao nosso planeta, controla a gravidade que segura os planetas ao seu redor e é essencial para a existência de toda a vida.";
+          texto.innerText="Por meio da fusão nuclear em seu núcleo, onde o hidrogênio é transformado em hélio, liberando muita energia.";
           descrevefigura.innerText="Imagem do Sol na cor amarela ";
           break;
      case "7":
